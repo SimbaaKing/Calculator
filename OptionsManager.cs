53e152dabbf00f385f1b18712fd4835fd4ac93fe
SimbaaKing <bozikovagabriela400@gmail.com>
2022-01-24 23:25:35 +0200

    comm2

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -5,14 +5,20 @@ namespace GitExercise
     public static class OptionsManager
     {
         public static string[] OptionsList = {
-            "a - Add"
+            "a - Add",
+            "s - Subtract"
+            
         };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
-
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
        
+
     }
 }
